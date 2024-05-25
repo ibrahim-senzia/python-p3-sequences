f32b8e5cf04a83b32e25720fca98e6152c094d92
Ibrahim Senzia <ibrahimsenzia@gmail.com>
2024-05-25 10:26:27 +0300

    sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    #fib = num of the last two nums
+    fib = [0,1]
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        for i in range(2,length):
+            fib.append(fib[i - 1] + fib[i - 2])
+        print(fib)
+
+print_fibonacci(9)
+
